15ca7fa5f3d449b9114dca1ab3304387be6b3d90
who_am_i <who_am_i@who-am-is-MacBook-Pro.local>
2023-02-09 21:46:21 +0530

    commited second phase

--- lib/view/category_display_screen.dart
+++ lib/view/category_display_screen.dart
@@ -1,3 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:eshopee/data/data.dart';
+import 'package:eshopee/widgets/top_container.dart';
 import 'package:flutter/material.dart';
 
 class CategoryDisplayPage extends StatefulWidget {
@@ -10,8 +13,80 @@ class CategoryDisplayPage extends StatefulWidget {
 class _CategoryDisplayPageState extends State<CategoryDisplayPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Category"),
+    return SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+      child: Column(
+        children: [
+          const TopContainer(
+            title: "Category",
+            searchBarTitle: "Search Category",
+          ),
+          ListView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: categories.length,
+            itemBuilder: (context, index) {
+              return Container(
+                width: MediaQuery.of(context).size.width * 0.80,
+                height: 170,
+                margin: const EdgeInsets.only(
+                  bottom: 10.0,
+                  left: 10.0,
+                  right: 10.0,
+                ),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: CachedNetworkImageProvider(
+                      categories[index].thumbnailImage,
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                foregroundDecoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.black.withOpacity(0.2),
+                      Colors.transparent,
+                    ],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.center,
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      bottom: 10.0,
+                      left: 10.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            categories[index].categoryName,
+                            style: const TextStyle(
+                                fontSize: 18.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 5),
+                          Text(
+                            "${categories[index].productCount} Products",
+                            style: const TextStyle(
+                                fontSize: 12.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          )
+        ],
+      ),
     );
   }
 }
--- lib/view/product_display_page.dart
+++ lib/view/product_display_page.dart
@@ -1,3 +1,5 @@
+import 'package:eshopee/utils/colors.dart';
+import 'package:eshopee/widgets/product_display_list_view.dart';
 import 'package:eshopee/widgets/top_container.dart';
 import 'package:flutter/material.dart';
 
@@ -8,14 +10,108 @@ class ProductDisplayPage extends StatefulWidget {
   State<ProductDisplayPage> createState() => _ProductDisplayPageState();
 }
 
-class _ProductDisplayPageState extends State<ProductDisplayPage> {
+class _ProductDisplayPageState extends State<ProductDisplayPage>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+  int selectedValue = 0;
+  int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this, initialIndex: 0);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    tabController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
       child: Column(
-        children: const [
-          TopContainer(title: "title", searchBarTitle: "searchBarTitle")
+        children: [
+          const TopContainer(
+            title: "E-SHOPEE",
+            searchBarTitle: "Search Product",
+          ),
+          TabBar(
+            controller: tabController,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorColor: Colors.transparent,
+            labelColor: Colors.white,
+            unselectedLabelColor: kBackgroundColor,
+            onTap: (value) {
+              setState(() {
+                selectedValue = value;
+              });
+              tabController.animateTo(value);
+            },
+            tabs: [
+              Container(
+                width: double.infinity,
+                height: 50,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: selectedValue == 0
+                        ? kBackgroundColor
+                        : kGreyColor.withOpacity(0.8),
+                    boxShadow: selectedValue == 0
+                        ? [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.8),
+                              blurRadius: 5,
+                              spreadRadius: 1,
+                              offset: const Offset(0, 1),
+                            ),
+                          ]
+                        : null),
+                child: const Text(
+                  "Trending",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 50,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: selectedValue == 1
+                        ? kBackgroundColor
+                        : kGreyColor.withOpacity(0.8),
+                    boxShadow: selectedValue == 1
+                        ? [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.8),
+                              blurRadius: 5,
+                              spreadRadius: 1,
+                              offset: const Offset(0, 1),
+                            ),
+                          ]
+                        : null),
+                child: const Text(
+                  "Clothing",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: TabBarView(
+              controller: tabController,
+              physics: const NeverScrollableScrollPhysics(),
+              children: const [
+                ProductDisplayWidget(),
+                ProductDisplayWidget(),
+              ],
+            ),
+          ),
         ],
       ),
     );
